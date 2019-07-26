a14e4e6fda34b54e747c08ae6ca7b8f6d9448dc0
oukann <460982416@qq.com>
2019-07-26 10:39:41 +0900

    add function
    (cherry picked from commit 2312535f7a1a2441013b34b545cc226fbc721ad0)

---
+++ MySpringBootProject/src/main/java/com/sys/entity/UserEntity.java
@@ -0,0 +1,35 @@
+package com.sys.entity;
+
+public class UserEntity {
+	private Integer id;
+	private String loginName;
+	private String passWord;
+	private String roleId;
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getLoginName() {
+		return loginName;
+	}
+	public void setLoginName(String loginName) {
+		this.loginName = loginName;
+	}
+	public String getPassWord() {
+		return passWord;
+	}
+	public void setPassWord(String passWord) {
+		this.passWord = passWord;
+	}
+	public String getRoleId() {
+		return roleId;
+	}
+	public void setRoleId(String roleId) {
+		this.roleId = roleId;
+	}
+	
+	
+}
